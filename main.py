73dc6762b95ea11f482a908d0b7c0b725e014cc1
Arnis Bartusevicius <arnisbartusevicius@proton.me>
2023-08-02 09:55:08 +0100

    Make code repeat and update comments

--- main.py
+++ main.py
@@ -3,22 +3,17 @@ import time # Let's us wait before executing another line of code
 import easygui # Let's you choose file directory
 import os.path # Let's us check inside a text file or folder
 
-if os.path.isfile("./filedir.txt") == False or os.stat("filedir.txt").st_size == 0:  # Checks if filedir.txt exists or if it's empty
+if os.path.isfile("./filedir.txt") == False or os.stat("filedir.txt").st_size == 0:  # If filedir.txt doesn't exist or it's empty:
     filedir = open("filedir.txt", "w") # Opens filedir.txt in "write" mode (This also creates filedir.txt)
     path = easygui.diropenbox() # Opens up a GUI to select a folder
     filedir.write(f"{path}\image.png") # Writes the path that you selected to filedir.txt
     filedir.close() # Closes filedir.txt
-    
-print("Saving Image...") # Displays that it's saving the image in the terminal
-img = ImageGrab.grabclipboard() # Grabs image from clipboard
 
-if isinstance(img, Image.Image) == False: # If clipboard doesn't have an image:
-    print("You do not have an image saved in your clipboard.") # Tells the user that they don't have an image in the clibpard
-    time.sleep(1) # Waits one second
-    quit() # Closes the program
-else: # If clipboard has an image:
-    filedir = open("filedir.txt", "r") # Opens filedir.txt in read mode
-    img.save(filedir.read()) # Saves image in the path located in filedir.txt
-    print("Image saved") # Displays that the image was saved in the terminal
-    time.sleep(1) # Waits one second
-    quit() # Exits the program after waiting 1 second
+while True: # This while statement runs forever; repeating the code
+    img = ImageGrab.grabclipboard() # Grabs image from clipboard
+    if isinstance(img, Image.Image) == False: # If clipboard doesn't have an image:
+        time.sleep(1) # Waits one second
+    else: # If clipboard has an image:
+        filedir = open("filedir.txt", "r") # Opens filedir.txt in read mode
+        img.save(filedir.read()) # Saves image in the path located in filedir.txt
+        time.sleep(1) # Waits one second
